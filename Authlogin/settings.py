0e815efe3c2e5c6022bea6c6eb1f0a3da4b7ab40
esangayt <24_de_7@protonmail.com>
2024-08-07 01:29:22 -0500

    Prueba de auth

--- Authlogin/settings.py
+++ Authlogin/settings.py
@@ -141,3 +141,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'esangays21fe@gmail.com'
 EMAIL_HOST_PASSWORD = 'zsbu lnlm zxkf xxva'
 EMAIL_PORT = 587
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
--- Authlogin/urls.py
+++ Authlogin/urls.py
@@ -17,11 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from Authlogin.views import HelloWorldView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("api/v1/dj-rest-auth/", include("dj_rest_auth.urls")),  # new
+    path("test/", HelloWorldView.as_view(), name="hello_world"),
     path("api/v1/dj-rest-auth/registration/",  # new
          include("dj_rest_auth.registration.urls")),
+    path("accounts/", include("allauth.urls")),
+    path("_allauth/", include("allauth.headless.urls")),
 
 ]
